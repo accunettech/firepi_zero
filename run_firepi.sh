64b670549fd4abe40d349c75cf468a3e0f945f86
Chris Pilotto <chrispilotto@gmail.com>
2025-09-29 12:16:14 -0400

    cleanup

--- run_firepi.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-cd "$(dirname "$0")"
-
-if [[ -x ".venv/bin/python" ]]; then
-  exec ".venv/bin/python" -u app.py
-else
-  exec /usr/bin/python3 -u app.py
-fi
