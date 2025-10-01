0d08b39d376f590849b026b017394f921e1986a9
Chris Pilotto <chrispilotto@gmail.com>
2025-09-30 20:22:29 -0400

    wifi setup improvements

--- manual.sh
+++
@@ -1,4 +0,0 @@
-APP_HOME=/home/chris/firepi
-echo '{"ssid":"wSecLan","psk":"sweetac!"}' > "$APP_HOME/instance/pending_wifi.json"
-APP_HOME="$APP_HOME" FIREPI_PENDING_WIFI="$APP_HOME/instance/pending_wifi.json" \
-  "$APP_HOME/wifi_scripts/firepi-wifi-switch.sh"
